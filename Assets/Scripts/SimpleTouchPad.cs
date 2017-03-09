34bf20a75967896b6d58e74880300d45c480a1c4
Andrew Bell <fiixed@gmail.com>
2017-03-08 19:50:47 -0600

    fix:  pointerid double touch protection

--- Assets/Scripts/SimpleTouchPad.cs
+++ Assets/Scripts/SimpleTouchPad.cs
@@ -12,28 +12,38 @@ public class SimpleTouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, 
     private Vector2 origin;
     private Vector2 direction;
     private Vector2 smoothDirection;
+    private bool touched;
+    private int pointerID;
 
     private void Awake() {
         direction = Vector2.zero;
+        touched = false;
     }
 
     public void OnPointerDown(PointerEventData eventData) {
         // Set our start point
-        origin = eventData.position;
+        if (!touched) {
+            touched = true;
+            pointerID = eventData.pointerId;
+            origin = eventData.position;
+        }      
     }
 
     public void OnDrag(PointerEventData eventData) {
         // Compare the difference between our start point and current pointer pos
-        Vector2 currentPosition = eventData.position;
-        Vector2 directionRaw = currentPosition - origin;
-        direction = directionRaw.normalized;
-        Debug.Log(direction);
+        if (eventData.pointerId == pointerID) {
+            Vector2 currentPosition = eventData.position;
+            Vector2 directionRaw = currentPosition - origin;
+            direction = directionRaw.normalized;
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData) {
         // Reset everything
-        direction = Vector2.zero;
-
+        if (eventData.pointerId == pointerID) {
+            direction = Vector2.zero;
+            touched = false;
+        }
     }
 
     public Vector2 GetDirection () {
